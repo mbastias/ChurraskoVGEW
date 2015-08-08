98d1fd20f47f8f890536edf95a5084aa13c1768a
Marcelo Bastias <marcelo.bastias.o@gmail.com>
2015-08-08 18:22:34 -0300

    Movimiento Player

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttack : MonoBehaviour {
+
+	public GameObject attackGameObject;
+	GameObject[] slowAttacks;
+	public float attackVelocity;
+	int lastSpawned, newSpawn;
+	int attackQuantity;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+		lastSpawned = 0;
+		newSpawn = -1;
+		attackQuantity = 3;
+
+		anim = GetComponent<Animator> ();
+		anim.SetBool("isAttacking",false);
+
+		slowAttacks = new GameObject[attackQuantity];
+		for (int i=0; i<attackQuantity; i++) {
+
+			slowAttacks[i] = Instantiate(attackGameObject,Vector3.zero, Quaternion.identity) as GameObject;
+			slowAttacks[i].SetActive(false);
+
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (anim.GetBool ("isAttacking"))
+			anim.SetBool ("isAttacking", false);
+
+		if (Input.GetKeyDown (KeyCode.C)) {
+
+			anim.SetBool("isAttacking",true);
+			spawnAttack();
+		}
+			//
+	
+	}
+
+	void spawnAttack() {
+
+		newSpawn = lastSpawned % attackQuantity;
+
+		slowAttacks [newSpawn].SetActive (true);
+		slowAttacks [newSpawn].transform.position = transform.FindChild ("Attack Point").transform.position;
+		Rigidbody2D rgAttack = slowAttacks [newSpawn].GetComponent<Rigidbody2D> ();
+
+		if (transform.localScale.x > 0) {
+			rgAttack.velocity = new Vector2 (attackVelocity, 0);
+			slowAttacks [newSpawn].transform.localScale = 
+				new Vector3 (Mathf.Abs (slowAttacks [newSpawn].transform.localScale.x), slowAttacks [newSpawn].transform.localScale.y,slowAttacks[newSpawn].transform.localScale.z);
+		}
+
+		else {
+			slowAttacks[newSpawn].transform.localScale = 
+				new Vector3(-Mathf.Abs(slowAttacks[newSpawn].transform.localScale.x),slowAttacks[newSpawn].transform.localScale.y,slowAttacks[newSpawn].transform.localScale.z);
+			rgAttack.velocity = new Vector2 (-attackVelocity, 0);
+
+		}
+		if (lastSpawned == 100)
+			lastSpawned = 0;
+		lastSpawned++;
+
+	}
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	private Rigidbody2D rg;
+	public Transform lineEndRight;
+	public Transform lineStartRight;
+	//public Transform lineEndLeft;
+	//public Transform lineStartLeft;
+	public float MoveSpeed;
+	private float direction;
+	private float movement;
+	public float jumpHeight;
+	public bool isGrounded;
+	private Animator anim;
+
+
+	// Use this for initialization
+	void Awake () {
+	
+		rg = GetComponent<Rigidbody2D>();
+		anim = GetComponent<Animator>();
+		isGrounded = false;
+	
+	}
+	
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+	
+	void FixedUpdate () 
+	{
+		anim.SetFloat("SpeedX",rg.velocity.x);
+		Raycast();	
+		if (rg.velocity.x != 0) {
+			anim.SetBool ("isWalking", true);
+			
+			
+			
+			if (rg.velocity.x > 0) 
+				transform.localScale = new Vector3 (Mathf.Abs (transform.localScale.x), transform.localScale.y, transform.localScale.z);
+			
+			else if (rg.velocity.x < 0)
+				transform.localScale = new Vector3 (-Mathf.Abs (transform.localScale.x), transform.localScale.y, transform.localScale.z);
+		} else {
+			anim.SetBool ("isWalking", false);
+		}
+		
+		if(Input.GetButtonDown("Jump"))
+		{
+			jump();
+		}
+		
+		direction = Input.GetAxis("Horizontal");
+		rg.velocity = new Vector2(direction*MoveSpeed,rg.velocity.y);
+	}
+
+	void Raycast()
+	{
+		Debug.Log("hola");	
+		Debug.DrawLine(lineStartRight.position,lineEndRight.position,Color.red);
+		//Debug.DrawLine(lineStartLeft.position,lineEndLeft.position,Color.red);
+		
+		isGrounded = Physics2D.Linecast(lineStartRight.position,lineEndRight.position,1 << LayerMask.NameToLayer("Ground")) ;
+					//|| Physics2D.Linecast(lineStartLeft.position,lineEndLeft.position,1 << LayerMask.NameToLayer("Ground")) ;
+			
+			
+	}
+
+	public void jump() {
+
+		if (isGrounded) {
+			//rg.velocity = new Vector2 (rg.velocity.x, jumpHeight);
+			rg.AddForce(Vector2.up * jumpHeight);
+		}
+	}
+
+
+	
+}
