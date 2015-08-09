232e12b94a9346f3e36947bafceab536dc339c1f
Oscar Rencoret <o.rencoret@gmail.com>
2015-08-09 03:58:36 -0300

    Cameras

---
+++ Assets/Scripts/Camera/CameraSplit.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSplit : MonoBehaviour {
+
+	public GameObject Player1;
+	public GameObject Player2;
+	private Camera CameraObject;
+
+	// Use this for initialization
+	void Start () {
+		CameraObject = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Player1.transform.position.x > Player2.transform.position.x) {
+			Vector2 position = new Vector2(.5f, 0);
+			Vector2 size = new Vector2(.5f, 1);
+			CameraObject.rect = new Rect(position, size);
+		} else {
+			Vector2 position = new Vector2(0, 0);
+			Vector2 size = new Vector2(.5f, 1);
+			CameraObject.rect = new Rect(position, size);
+		}
+		FollowPlayer ();
+	}
+
+	void FixedUpdate(){
+	}
+
+	void FollowPlayer(){
+		Vector3 CameraPosition = transform.position;
+		Vector3 Player1Position = Player1.transform.position;
+		Player1Position.y += 2;
+		transform.position = new Vector3 (Player1Position.x, Player1Position.y, CameraPosition.z);
+	}
+}
---
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour {
+
+	public GameObject Player1;
+	public GameObject Player2;
+	private Camera CameraComponent;
+
+	// Use this for initialization
+	void Start () {
+		CameraComponent = GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (Player1.transform.position, Player2.transform.position) < 10f) {
+			CameraComponent.depth = 3;
+		} else {
+			CameraComponent.depth = 0;
+		}
+		FollowPlayers ();
+	}
+
+	void FollowPlayers(){
+		Vector3 CameraPosition = transform.position;
+		Vector3 Player1Position1 = Player1.transform.position;
+		Vector3 Player1Position2 = Player2.transform.position;
+		Vector3 NewPosition = Vector3.zero;
+
+		if (Player1Position1.y > Player1Position2.y) {
+			NewPosition.y = (Player1Position1.y + Player1Position2.y) / 2f;
+		} else {
+			NewPosition.y = (Player1Position2.y + Player1Position1.y) / 2f;
+		}
+
+		if (Player1Position1.x > Player1Position2.x) {
+			NewPosition.x = (Player1Position1.x + Player1Position2.x) / 2f;
+		} else {
+			NewPosition.x = (Player1Position2.x + Player1Position1.x) / 2f;
+		}
+
+		NewPosition.z = CameraPosition.z;
+
+		Debug.Log (NewPosition);
+
+		transform.position = NewPosition;
+	}
+}
