e999132f51bf4460f4af4854b94278bca585f63d
Oscar Rencoret <o.rencoret@gmail.com>
2015-08-09 14:46:10 -0300

    Main GUI

---
+++ Assets/Scripts/GUI/GUIMainScreen.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIMainScreen : MonoBehaviour {
+	public void Play(string buttonID){
+		Application.LoadLevel ("LevelFinal1");
+	}
+
+	public void ExitGame(){
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/GUI/JisusGUI.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JisusGUI : MonoBehaviour {
+
+	private float fromx = -7f;
+	private float tox = -1f;
+	private bool direction;
+
+	// Use this for initialization
+	void Start () {
+		direction = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (direction && transform.position.x < tox) {
+			Vector3 actual = transform.position;
+			actual.x += .1f;
+			transform.localScale = new Vector3(-.5f, .5f, 1);
+			transform.position = actual;
+		} else {
+			direction = false;
+		}
+		if (!direction && transform.position.x > fromx) {
+			Vector3 actual = transform.position;
+			actual.x -= .1f;
+			transform.localScale = new Vector3(.5f, .5f, 1);
+			transform.position = actual;
+		} else {
+			direction = true;
+		}
+	}
+}
